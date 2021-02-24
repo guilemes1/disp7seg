223376cd942ee601578b6a85398b34934fbd0ff2
20185189 <20185189@DSN1271158619.cfp127aluno.local>
2021-02-24 14:10:01 -0300

    Primeiro commit. Projeto Display 7 segmentos.

---
+++ disp7seg.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: 20185189
+ *
+ * Created on 24 de Fevereiro de 2021, 14:07
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "delay.h"
+
+void main(void) 
+{
+    while(1)
+    {
+    
+    }
+}

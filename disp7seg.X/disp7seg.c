b1941dc666aeb9654edbc90ea82e2bec1aa05ba1
20185189 <20185189@DSN1271158619.cfp127aluno.local>
2021-02-24 16:15:24 -0300

    Implementação da biblioteca "disp7seg"

---
+++ disp7seg.X/disp7seg.c
@@ -0,0 +1,28 @@
+/*
+ * File:   disp7seg.c
+ * Author: 20185189
+ *
+ * Created on 24 de Fevereiro de 2021, 15:43
+ */
+
+#include "config.h"
+#include "delay.h"
+#include <xc.h>
+
+char vetor[16] = { 0x3F, 0x06, 0x5B, 0x4F,
+                   0x66, 0x6D, 0x7D, 0x07,
+                   0x7F, 0x6F, 0x77, 0x7C,
+                   0x39, 0x5E, 0x79, 0x71};
+
+void disp7seg_init(void)
+{
+    TRISD = 0x00;
+    PORTD = 0x00;
+}
+
+void disp7seg(int digito)
+{
+    PORTD = vetor[digito];
+    delay(1000);
+}
+
--- disp7seg.X/main.c
+++ disp7seg.X/main.c
@@ -5,15 +5,21 @@
  * Created on 24 de Fevereiro de 2021, 14:07
  */
 
-
+#include "disp7seg.h"
 #include <xc.h>
 #include "config.h"
 #include "delay.h"
 
 void main(void) 
 {
+    int cont = 0;
+    disp7seg_init();
+    
     while(1)
     {
-    
+        disp7seg(cont);
+        cont++;
+        if(cont >= 16)
+            cont = 0;
     }
 }
